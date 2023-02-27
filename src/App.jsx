e69c04e0a7feddf94efdceda17d0d23b4ae24aa0
Ynot Alcaraz <a19490902@itmexicali.edu.mx>
2023-02-26 19:30:17 -0800

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export const App = () => {
     <>
         <Navbar/>
         <Routes>
-            <Route path='productos' element={<Catalogos title={'Productos'}/>}/>
+            <Route path='productos' element={<ProductsTable/>}/>
             <Route path='clientes' element={<Catalogos title={'Clientes'}/>}/>
             <Route path='proveedores' element={<Catalogos title={'Proveedores'}/>}/>
             <Route path='pagos' element={<Catalogos title={'Pagos'}/>}/>
@@ -17,7 +17,7 @@ export const App = () => {
 
             <Route path='/' element={<Navigate to='productos'/>}/>
         </Routes>
-        <ProductsTable/>
+        
     </>
   )
 }
--- src/components/catalogos/ProductsTable.jsx
+++ src/components/catalogos/ProductsTable.jsx
@@ -20,37 +20,39 @@ function ProductsTable() {
   }, []);
 
   return (
-    <div className="container">
-      <h2 className="mt-3 mb-3">Products Table</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Código de Barras</th>
-            <th>Descripción</th>
-            <th>Existencia en Inventario</th>
-            <th>Imagen del Producto</th>
-            <th>Nombre</th>
-            <th>Precio de Venta</th>
-            <th>Unidad de Medida</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map(product => (
-            <tr key={product.id}>
-              <td>{product.codigo_de_barras}</td>
-              <td>{product.descripcion}</td>
-              <td>{product.existencia_en_inventario}</td>
-              <td>
-                <img src={product.imagen_del_producto} alt={product.nombre} width="100px" className="img-thumbnail" />
-              </td>
-              <td>{product.nombre}</td>
-              <td>{product.precio_de_venta}</td>
-              <td>{product.unidad_de_medida}</td>
+    <>
+      <h1>Tipos de Producto</h1>
+      <div className="container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Código de Barras</th>
+              <th>Descripción</th>
+              <th>Existencia en Inventario</th>
+              <th>Imagen del Producto</th>
+              <th>Nombre</th>
+              <th>Precio de Venta</th>
+              <th>Unidad de Medida</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {products.map(product => (
+              <tr key={product.id}>
+                <td>{product.codigo_de_barras}</td>
+                <td>{product.descripcion}</td>
+                <td>{product.existencia_en_inventario}</td>
+                <td>
+                  <img src={product.imagen_del_producto} alt={product.nombre} width="100px" className="img-thumbnail" />
+                </td>
+                <td>{product.nombre}</td>
+                <td>{product.precio_de_venta}</td>
+                <td>{product.unidad_de_medida}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
