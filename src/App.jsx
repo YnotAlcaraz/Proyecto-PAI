37238d6842aefcd640ad498535fa48dd0bc16a2d
Ynot Alcaraz <a19490902@itmexicali.edu.mx>
2023-03-12 03:38:24 -0700

    Sidebar repiola ctm brandon me borraste mis branches

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import ApiTable from "./components/ApiTable";
-import { CatalogoEmpleados } from "./components/catalogos/CatalogoEmpleados";
-import { CatalogoPagos } from "./components/catalogos/CatalogoPagos";
-import { CatalogoProductos } from "./components/catalogos/CatalogoProductos";
-import { CatalogoProveedores } from "./components/catalogos/CatalogoProveedores";
-import { Navbar } from "./components/Navbar";
+import { Sidebar } from "./Sidebar";
+
 
 export const App = () => {
   return (
     <>
-      <Navbar />
+      <Sidebar />
       <Routes>
-        <Route path="productos" element={<CatalogoProductos />} />
+        {/* <Route path="productos" element={<CatalogoProductos />} />
         <Route path="proveedores" element={<CatalogoProveedores />} />
         <Route path="pagos" element={<CatalogoPagos />} />
         <Route path="empleados" element={<CatalogoEmpleados />} />
-
-        <Route path="/" element={<Navigate to="productos" />} />
+        <Route path="/" element={<Navigate to="productos" />} /> */}
       </Routes>
-      <ApiTable />
     </>
   );
 };
