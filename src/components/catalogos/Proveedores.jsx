66f0853999459707efe2fc674a777413c9e97b3f
Brandon Hernandez <a19490920@itmexicali.edu.mx>
2023-03-12 16:37:02 -0700

    xde

--- src/components/catalogos/Proveedores.jsx
+++ src/components/catalogos/Proveedores.jsx
@@ -1,9 +1,26 @@
 import useTable from "../customHook/useTable";
+import {
+  Table,
+  Button,
+  Popconfirm,
+  Modal,
+  Form,
+  Input,
+  DatePicker,
+  Select,
+  Col,
+  Row,
+} from "antd";
 export const Proveedores = () => {
   return (
-    <div>
-      Proveedores
+    <>
+      <h1>Catálogo De Productos</h1>
+      <hr />
+      <Button type="primary" style={{ marginBottom: 20 }}>
+        Agregar Producto
+      </Button>
+
       {useTable("http://localhost:3000/proveedores")}
-    </div>
+    </>
   );
 };
